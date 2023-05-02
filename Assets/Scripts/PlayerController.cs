6bd2059689231bb8e46699ea2c4fc0672b6bac4d
Luis-Collazo <lcolla2362@gmail.com>
2023-05-01 21:02:56 -0400

    Ton of Work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
     
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,12 @@ void Update()
           Vector3 movement = new Vector3 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
      
           transform.position += movement * Time.deltaTime * speed;
+
+          if (Input.GetKeyDown(KeyCode.Space))
+          {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+          }
  }
 
  
